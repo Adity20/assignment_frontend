e790ed078e65fc9eace538b8a0d43eacce94ceb2
Aditya Sharma <sharma.aditya9164@gmail.com>
2024-10-10 11:45:26 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ const CryptoStats = () => {
 
   useEffect(() => {
     fetchData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedCoin]);
 
   const fetchData = async () => {
